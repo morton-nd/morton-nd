9a04e26e38ad71a589c75526d17698eec69f0dc9
Kevin Hartman <kevin@hart.mn>
2015-02-23 17:42:18 -0800

    Initial Commit

---
+++ MetaMorton/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MetaMorton
+//
+//  Created by Kevin Hartman on 2/23/15.
+//  Copyright (c) 2015 Kevin Hartman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
