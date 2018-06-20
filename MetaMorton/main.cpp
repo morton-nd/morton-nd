a45f3ba41970cf119bb622107da530f13b390c59
Kevin Hartman <kevin@hart.mn>
2018-06-20 17:03:20 -0400

    Update main client code.

--- MetaMorton/main.cpp
+++ MetaMorton/main.cpp
@@ -10,25 +10,14 @@
 
 #include "mortoncode.h"
 
+auto& morton = Morton<3, 4, uint32_t, uint32_t>;
+const auto test = morton.Encode(5, 9, 1);
+
 int main(int argc, const char * argv[]) {
 
-//    const int range = 256;
-//
-//    for (int i = 0; i < range; i++) {
-//        printf("Value:%d\n", Array<range>::Value[i]);
-//    }
-
-    auto& morton = Morton<3, 8, uint32_t, uint32_t>;
-
     //MortonCode<3, 8, uint8_t, uint32_t> morton;
-    //using MyMorton = Morton<3, 8, uint32_t, uint32_t>;
-    
-    for (int i = 0; i < 256; i++) {
-        uint32_t split = morton.LookupTable[i];
-        
-        //Member<6, 1 << 6> test;
-        //printf("Value:%d\n", test.array[1]);
-        printf("Value:0x%2x\n", split);
-    }
+    //using MyMorton = Morton<3, 8, uint32_t, uint32_t>
+    printf("Encoding of (1, 2, 3): %d\n", test);
+
     return 0;
 }
