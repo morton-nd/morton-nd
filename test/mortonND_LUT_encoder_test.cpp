a27da28041f5676bd87a10ea1631b36ce4e18e54
Kevin Hartman <kevin@hart.mn>
2018-09-20 03:13:58 -0400

    s

--- test/mortonND_LUT_encoder_test.cpp
+++ test/mortonND_LUT_encoder_test.cpp
@@ -3,63 +3,75 @@
 #include "mortonND_test_control.h"
 #include "mortonND_test_common.h"
 
-constexpr auto Morton_1_12 = mortonnd::MortonNDLutEncoder<3, 12, 12>();
-constexpr auto Morton_2_6 = mortonnd::MortonNDLutEncoder<3, 12, 6>();
-constexpr auto Morton_3_4 = mortonnd::MortonNDLutEncoder<3, 12, 4>();
-constexpr auto Morton_4_3 = mortonnd::MortonNDLutEncoder<3, 12, 3>();
-constexpr auto Morton_6_2 = mortonnd::MortonNDLutEncoder<3, 12, 2>();
-constexpr auto Morton_12_1 = mortonnd::MortonNDLutEncoder<3, 12, 1>();
+template<size_t FieldBits, typename Ret, typename ...Fields, typename std::enable_if<sizeof...(Fields) < 6, int>::type = 0>
+bool TestEncode(const std::function<Ret(Fields...)>& function) {
+	return TestEncodeFunction<FieldBits>(function);
+}
 
-template<size_t FieldBits, typename Ret, typename ...Fields, size_t ...N, size_t ...FieldIdx>
-bool TestMortonNDLutSet0(std::index_sequence<N...>, std::index_sequence<FieldIdx...>) {
+template<size_t FieldBits, typename Ret, typename ...Fields, typename std::enable_if<sizeof...(Fields) >= 6, int>::type = 0>
+bool TestEncode(const std::function<Ret(Fields...)>& function) {
+	std::cout << "  Using simple test." << std::endl;
+	return TestEncodeFunctionSimple<FieldBits>(function);
+}
+
+template<size_t FieldBits, typename Ret, typename ...Fields, size_t ...FieldIdx, size_t ...N>
+bool TestMortonNDLutSet(type_sequence<Fields...>, std::index_sequence<FieldIdx...>, std::index_sequence<N...>) {
 	static const auto luts = std::make_tuple(mortonnd::MortonNDLutEncoder<sizeof...(Fields), FieldBits, (N + 1)>()...);
 	static const auto funcs = std::make_tuple(
 		std::function<Ret(Fields...)>(std::bind(static_cast<Ret(mortonnd::MortonNDLutEncoder<sizeof...(Fields), FieldBits, (N + 1)>::*)(Fields...) const>(&mortonnd::MortonNDLutEncoder<sizeof...(Fields), FieldBits, (N + 1)>::Encode), std::get<N>(luts), variadic_placeholder<FieldIdx>{}...))...
 	);
+
 	return Reduce(std::logical_and<bool>{}, 
-		(std::cout << "Testing:  Fields = " << sizeof...(Fields) << ", Bits per field = " << FieldBits << ", Bits per LUT entry = " << (N + 1) << std::endl, TestEncodeFunction<Ret, FieldBits>(std::get<N>(funcs)))...
+		(std::cout << "Testing:  Fields = " << sizeof...(Fields) << ", Bits per field = " << FieldBits << ", Bits per LUT entry = " << (N + 1) << std::endl, TestEncode<FieldBits>(std::get<N>(funcs)))...
 	);
 }
 
-template<size_t FieldBits, typename Ret, typename ...Fields>
-bool TestMortonNDLutSet0(type_sequence<Fields...>) {
-	return TestMortonNDLutSet0<FieldBits, Ret, Fields...>(std::make_index_sequence<FieldBits>{}, std::make_index_sequence<sizeof...(Fields)>{});
-}
-
-template<size_t Fields, typename T, size_t FieldBits = std::numeric_limits<T>::digits / Fields>
+template<size_t Fields, size_t Digits, size_t FieldBits = Digits / Fields>
 bool TestMortonNDLutSet() {
-	return TestMortonNDLutSet0<FieldBits, T>(make_type_sequence<Fields, T>());
-}
-
-constexpr uint64_t Morton_2_6_Encode(uint64_t x, uint64_t y, uint64_t z) {
-	return Morton_2_6.Encode(x, y, z);
+	// (LUT size doesn't affect the type. 1 is arbitrary.)
+	using T = typename mortonnd::MortonNDLutEncoder<Fields, FieldBits, 1>::type;
+	return TestMortonNDLutSet<FieldBits, T>(
+			make_type_sequence<Fields, T>(), std::make_index_sequence<Fields>{}, std::make_index_sequence<FieldBits>{});
 }
 
 bool mortonnd_lut::TestEncode() {
-	//std::function<uint64_t(uint64_t, uint64_t, uint64_t)> f = &Morton_2_6_Encode;
-	//return TestEncodeFunction<uint64_t, 12>(f);
 	return Reduce(
 		std::logical_and<bool>{},
-		//TestMortonNDLutSet<1, uint64_t, 18>(),
-		TestMortonNDLutSet<2, uint64_t, 18>(), // Limit FieldBits to 18
-		TestMortonNDLutSet<3, uint64_t, 18>(),
-		TestMortonNDLutSet<4, uint64_t>(),
-		//TestMortonNDLutSet<12, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t>(),
 
-		TestMortonNDLutSet<2, uint32_t>(),
-		TestMortonNDLutSet<3, uint32_t>(),
-		TestMortonNDLutSet<4, uint32_t>(),
-		TestMortonNDLutSet<5, uint32_t>(),
+		// 1D
+		TestMortonNDLutSet<1, 64, 18>(), // Limit FieldBits to 18
+		TestMortonNDLutSet<1, 32, 18>(),
+		TestMortonNDLutSet<1, 16>(),
+		TestMortonNDLutSet<1, 8>(),
 
-		TestMortonNDLutSet<2, uint16_t>(),
-		TestMortonNDLutSet<3, uint16_t>(),
-		TestMortonNDLutSet<4, uint16_t>(),
-		TestMortonNDLutSet<5, uint16_t>(),
+		// 2D
+		TestMortonNDLutSet<2, 64, 18>(),
+		TestMortonNDLutSet<2, 32>(),
+		TestMortonNDLutSet<2, 16>(),
+		TestMortonNDLutSet<2, 8>(),
 
-		TestMortonNDLutSet<2, uint8_t>(),
-		TestMortonNDLutSet<3, uint8_t>(),
-		TestMortonNDLutSet<4, uint8_t>(),
-		TestMortonNDLutSet<5, uint8_t>()
+		// 3D
+		TestMortonNDLutSet<3, 64, 18>(),
+		TestMortonNDLutSet<3, 32>(),
+		TestMortonNDLutSet<3, 16>(),
+		TestMortonNDLutSet<3, 8>(),
 
+		// 4D
+		TestMortonNDLutSet<4, 64>(),
+		TestMortonNDLutSet<4, 32>(),
+		TestMortonNDLutSet<4, 16>(),
+		TestMortonNDLutSet<4, 8>(),
+
+		// 5D
+	    TestMortonNDLutSet<5, 64>(),
+		TestMortonNDLutSet<5, 32>(),
+		TestMortonNDLutSet<5, 16>(),
+		TestMortonNDLutSet<5, 8>(),
+
+		// 32 Dimensions
+		TestMortonNDLutSet<32, 64>(),
+
+		// 64 Dimensions
+		TestMortonNDLutSet<64, 64>()
 	);
 }
--- test/mortonND_test_common.h
+++ test/mortonND_test_common.h
@@ -6,12 +6,39 @@
 
 #include "mortonND_test_util.h"
 
-template <typename T, size_t FieldBits, size_t SliceBits = FieldBits < 4 ? FieldBits : 4, typename Ret, typename ...Fields>
+template <size_t FieldBits, typename Ret, typename ...Fields>
+static bool TestEncodeFunctionSimple(const std::function<Ret(Fields...)>& function) {
+    static const auto MaxValue = Pow(2, FieldBits);
+    static const auto FieldCount = sizeof...(Fields);
+    static_assert(FieldCount * FieldBits <= std::numeric_limits<uint64_t>::digits, "Control encoder cannot support > 64 bits.");
+    static_assert(std::numeric_limits<Ret>::digits >= FieldCount * FieldBits, "'morton' must support encoding width");
+
+    const std::function<bool(Fields...)> check_function = [&function](Fields...fields) -> bool {
+        const uint64_t correct = control_encode(fields...);
+        const Ret computed = function(fields...);
+
+        if (computed != correct) {
+            std::cout << "Mismatch when encoding";
+            PrintTuple(std::make_tuple(fields...), std::index_sequence_for<Fields...>{});
+            std::cout << "  Correct: " << correct << " Computed: " << computed << std::endl;
+        }
+
+        return computed == correct;
+    };
+
+    for (size_t i; i < MaxValue; i++) {
+        if (!check_function(static_cast<Fields>(i)...)) return false;
+    }
+
+    return true;
+};
+
+template <size_t FieldBits, size_t SliceBits = FieldBits < 4 ? FieldBits : 4, typename Ret, typename ...Fields>
 static bool TestEncodeFunction(const std::function<Ret(Fields...)>& function) {
 	static const auto FieldCount = sizeof...(Fields);
 	static_assert(FieldCount * FieldBits <= std::numeric_limits<uint64_t>::digits, "Control encoder cannot support > 64 bits.");
 	static_assert(FieldBits >= SliceBits, "At least 4 bits from each field must fit into 'morton'");
-	static_assert(std::numeric_limits<T>::digits >= FieldCount * FieldBits, "'morton' must support encoding width");
+	static_assert(std::numeric_limits<Ret>::digits >= FieldCount * FieldBits, "'morton' must support encoding width");
 
 	// For every set of 4 contiguous bits, test all possible values (0-15), with all other bits cleared
 	for (size_t offset = 0; offset <= FieldBits - SliceBits; offset++) {
@@ -19,10 +46,10 @@ static bool TestEncodeFunction(const std::function<Ret(Fields...)>& function) {
 			const uint64_t correct = control_encode((fields << offset)...);
 			const Ret computed = function((fields << offset)...);
 
-			//PrintTuple(std::make_tuple((fields << offset)...), std::index_sequence_for<Fields...>{});
-
 			if (computed != correct) {
-				std::cout << "  Correct: " << correct << " Computed: " << computed << std::endl;
+			    std::cout << "Mismatch when encoding";
+                PrintTuple(std::make_tuple((fields << offset)...), std::index_sequence_for<Fields...>{});
+                std::cout << "  Correct: " << correct << " Computed: " << computed << std::endl;
 			}
 
 			return computed == correct;
