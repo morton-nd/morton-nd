98bcebfd6edb750c556abd5654c5c8f4e4cf481a
Kevin Hartman <kevin@hart.mn>
2021-06-24 18:36:06 -0400

    Naming fix.

--- test/mortonND_LUT_test.cpp
+++ test/mortonND_LUT_test.cpp
@@ -4,7 +4,7 @@
 #include "variadic_placeholder.h"
 
 template<size_t FieldBits, typename Ret, typename ...Fields, size_t ...FieldIdx, size_t ...N>
-bool TestMortonNDLutSet(type_sequence<Fields...>, std::index_sequence<FieldIdx...>, std::index_sequence<N...>) {
+bool TestEncodeMortonNDLutSet(type_sequence<Fields...>, std::index_sequence<FieldIdx...>, std::index_sequence<N...>) {
     // Generate all LUTs at compile-time
     static constexpr auto luts = std::make_tuple(mortonnd::MortonNDLutEncoder<sizeof...(Fields), FieldBits, (N + 1)>()...);
 
@@ -23,12 +23,12 @@ bool TestMortonNDLutSet(type_sequence<Fields...>, std::index_sequence<FieldIdx..
 }
 
 template<size_t Fields, size_t Digits, size_t FieldBits = Digits / Fields>
-bool TestMortonNDLutSet() {
+bool TestEncodeMortonNDLutSet() {
     std::cout << "Testing " << Digits << "-bit " << Fields << "D LUT encoders (Bits/Field = " << FieldBits << ")..." << std::endl;
 
     // (LUT size doesn't affect the type. 1 is arbitrary.)
     using T = typename mortonnd::MortonNDLutEncoder<Fields, FieldBits, 1>::type;
-    return TestMortonNDLutSet<FieldBits, T>(
+    return TestEncodeMortonNDLutSet<FieldBits, T>(
         make_type_sequence<Fields, T>(), std::make_index_sequence<Fields>{}, std::make_index_sequence<FieldBits>{});
 }
 
@@ -37,43 +37,43 @@ bool mortonnd_lut::TestEncode() {
         std::logical_and<bool>{},
 
         // 1D
-        TestMortonNDLutSet<1, 64, 18>(), // Limit FieldBits to 18
-        TestMortonNDLutSet<1, 32, 18>(),
-        TestMortonNDLutSet<1, 16>(),
-        TestMortonNDLutSet<1, 8>(),
+        TestEncodeMortonNDLutSet<1, 64, 18>(), // Limit FieldBits to 18
+        TestEncodeMortonNDLutSet<1, 32, 18>(),
+        TestEncodeMortonNDLutSet<1, 16>(),
+        TestEncodeMortonNDLutSet<1, 8>(),
 
         // 2D
-        TestMortonNDLutSet<2, 64, 18>(),
-        TestMortonNDLutSet<2, 32>(),
-        TestMortonNDLutSet<2, 16>(),
-        TestMortonNDLutSet<2, 8>(),
+        TestEncodeMortonNDLutSet<2, 64, 18>(),
+        TestEncodeMortonNDLutSet<2, 32>(),
+        TestEncodeMortonNDLutSet<2, 16>(),
+        TestEncodeMortonNDLutSet<2, 8>(),
 
         // 3D
-        TestMortonNDLutSet<3, 64, 18>(),
-        TestMortonNDLutSet<3, 32>(),
-        TestMortonNDLutSet<3, 16>(),
-        TestMortonNDLutSet<3, 8>(),
+        TestEncodeMortonNDLutSet<3, 64, 18>(),
+        TestEncodeMortonNDLutSet<3, 32>(),
+        TestEncodeMortonNDLutSet<3, 16>(),
+        TestEncodeMortonNDLutSet<3, 8>(),
 
         // 4D
-        TestMortonNDLutSet<4, 64>(),
-        TestMortonNDLutSet<4, 32>(),
-        TestMortonNDLutSet<4, 16>(),
-        TestMortonNDLutSet<4, 8>(),
+        TestEncodeMortonNDLutSet<4, 64>(),
+        TestEncodeMortonNDLutSet<4, 32>(),
+        TestEncodeMortonNDLutSet<4, 16>(),
+        TestEncodeMortonNDLutSet<4, 8>(),
 
         // 5D
-        TestMortonNDLutSet<5, 64>(),
-        TestMortonNDLutSet<5, 32>(),
-        TestMortonNDLutSet<5, 16>(),
-        TestMortonNDLutSet<5, 8>(),
+        TestEncodeMortonNDLutSet<5, 64>(),
+        TestEncodeMortonNDLutSet<5, 32>(),
+        TestEncodeMortonNDLutSet<5, 16>(),
+        TestEncodeMortonNDLutSet<5, 8>(),
 
         // 8D
-        TestMortonNDLutSet<8, 16>(),
+        TestEncodeMortonNDLutSet<8, 16>(),
 
         // 32 Dimensions
-        TestMortonNDLutSet<32, 64>(),
+        TestEncodeMortonNDLutSet<32, 64>(),
 
         // 64 Dimensions
-        TestMortonNDLutSet<64, 64>()
+        TestEncodeMortonNDLutSet<64, 64>()
     );
 }
 
